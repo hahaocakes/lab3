ba873f6c48c4228ba04e16edbbbdb1be0d4e9933
Jasmine Tolentino <jatolentino3@student.apc.edu.ph>
2023-02-20 21:15:42 +0800

    Static

--- ci4/app/Controllers/Pages.php
+++ ci4/app/Controllers/Pages.php
@@ -2,12 +2,11 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Exceptions\PageNotFoundException; // Add this line
+
 class Pages extends BaseController
 {
-    public function index()
-    {
-        return view('welcome_message');
-    }
+    // ...
 
     public function view($page = 'home')
     {
